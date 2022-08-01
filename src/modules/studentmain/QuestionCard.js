1da69cc7a7a1a535186469abf0943ff9341c7a97
nhatngynxd <19110256@student.hcmute.edu.vn>
2022-08-01 16:11:30 +0700

    test

--- src/modules/studentmain/QuestionCard.js
+++ src/modules/studentmain/QuestionCard.js
@@ -1,51 +1,51 @@
-import React, { useState } from "react";
-import QuestionChoice from "./QuestionChoice";
-import QuestionText from "./QuestionText";
-import Swal from "sweetalert2";
-import { useDispatch } from "react-redux";
-import { setIsOpen } from "../../store/modal/modalSlice";
-import { setQuestion } from "../../store/question/questionSlice";
-import { useSelector } from "react-redux";
+import React, { useState } from 'react';
+import QuestionChoice from './QuestionChoice';
+import QuestionText from './QuestionText';
+import Swal from 'sweetalert2';
+import { useDispatch } from 'react-redux';
+import { setIsOpen } from '../../store/modal/modalSlice';
+import { setQuestion } from '../../store/question/questionSlice';
+import { useSelector } from 'react-redux';
 const QuestionCard = ({ item, index }) => {
   const { questionList } = useSelector((state) => state.questionList);
   const dispatch = useDispatch();
-  const [answer, setAnswer] = useState("no answer");
+  const [answer, setAnswer] = useState('no answer');
   const handleAnswer = (answer) => {
     setAnswer(answer);
   };
   const onSubmit = async (index) => {
-    if (answer === "no answer") {
+    if (answer === 'no answer') {
       Swal.fire({
-        title: "Warning",
-        text: "Please choose your answer",
-        icon: "warning",
-        confirmButtonColor: "#005CFF",
-        confirmButtonText: "OK",
+        title: 'Warning',
+        text: 'Please choose your answer',
+        icon: 'warning',
+        confirmButtonColor: '#005CFF',
+        confirmButtonText: 'OK',
       });
     } else {
       const question = { ...questionList[index] };
-      if (typeof question?.result == "boolean") {
+      if (typeof question?.result == 'boolean') {
         question.result = question?.result.toString();
       }
       Swal.fire({
-        title: "Are you sure?",
+        title: 'Are you sure?',
         text: "You won't be able to revert this!",
-        icon: "warning",
+        icon: 'warning',
         showCancelButton: true,
-        confirmButtonColor: "#1ed760",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Submit",
+        confirmButtonColor: '#1ed760',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Submit',
       }).then((result) => {
         if (result.isConfirmed) {
           if (answer == question?.result) {
             Swal.fire({
-              title: "Exactly!",
+              title: 'Exactly!',
               text: `Correct answer : ${question?.result}`,
-              icon: "success",
+              icon: 'success',
               showCancelButton: true,
-              cancelButtonColor: "#d33",
-              confirmButtonColor: "#005CFF",
-              confirmButtonText: "View explanation",
+              cancelButtonColor: '#d33',
+              confirmButtonColor: '#005CFF',
+              confirmButtonText: 'View explanation',
             }).then((result) => {
               if (result.isConfirmed) {
                 dispatch(setIsOpen(true));
@@ -54,13 +54,13 @@ const QuestionCard = ({ item, index }) => {
             });
           } else {
             Swal.fire({
-              title: "Wrong!",
+              title: 'Wrong!',
               text: `Correct answer : ${question?.result}`,
-              icon: "error",
+              icon: 'error',
               showCancelButton: true,
-              cancelButtonColor: "#d33",
-              confirmButtonColor: "#005CFF",
-              confirmButtonText: "View explanation",
+              cancelButtonColor: '#d33',
+              confirmButtonColor: '#005CFF',
+              confirmButtonText: 'View explanation',
             }).then((result) => {
               if (result.isConfirmed) {
                 dispatch(setIsOpen(true));
@@ -73,52 +73,48 @@ const QuestionCard = ({ item, index }) => {
     }
   };
   return (
-    <div className="flex flex-col items-start justify-between p-5 rounded-lg bg-white shadow gap-8">
-      <div className="flex items-center gap-2 text-lg w-full flex-wrap">
-        <p className="font-semibold">Question {index + 1}:</p>
+    <div className='flex flex-col items-start justify-between p-5 rounded-lg bg-white shadow gap-8'>
+      <div className='flex items-center gap-2 text-lg w-full flex-wrap'>
+        <p className='font-semibold'>Question {index + 1}:</p>
         <math-field
-          id="formula"
+          id='formula'
           read-only
           style={{
-            outline: "none",
-            fontSize: "18px",
-            flex: "1 1 0%",
-          }}
-        >
+            outline: 'none',
+            fontSize: '18px',
+            flex: '1 1 0%',
+          }}>
           {item?.question?.plaintext}
         </math-field>
       </div>
-      <div className="flex items-center justify-center w-full">
+      <div className='flex items-center justify-center w-full'>
         <img
           src={
             item?.question?.img
               ? `data:image/png;base64, ${item?.question?.img}`
-              : "https://img.freepik.com/premium-vector/person-people-question-mark-answer-illustration-concept-action_159757-131.jpg?w=1380"
+              : 'https://img.freepik.com/premium-vector/person-people-question-mark-answer-illustration-concept-action_159757-131.jpg?w=1380'
           }
-          alt=""
-          className="w-40"
+          alt=''
+          className='w-40'
         />
       </div>
 
-      <div className="w-full flex flex-col gap-3 text-sm">
-        {item.questionType === "TEXT" ? (
+      <div className='w-full flex flex-col gap-3 text-sm'>
+        {item.questionType === 'TEXT' ? (
           <QuestionText
             idQuestion={item.id}
-            handleAnswer={handleAnswer}
-          ></QuestionText>
+            handleAnswer={handleAnswer}></QuestionText>
         ) : (
           <QuestionChoice
             question={item}
-            handleAnswer={handleAnswer}
-          ></QuestionChoice>
+            handleAnswer={handleAnswer}></QuestionChoice>
         )}
         <div
-          className="w-full flex justify-end"
+          className='w-full flex justify-end'
           onClick={() => {
             onSubmit(index);
-          }}
-        >
-          <div className="button border-primary px-6 text-base">Submit</div>
+          }}>
+          <div className='button border-primary px-6 text-base'>Submit</div>
         </div>
       </div>
     </div>
